dbaa861777b0c67d5eb7656d57408f8968e9b90c
asharikov <xakepok@xakepok.com>
2019-01-23 13:46:15 +0300

    Initial commit

---
+++ user/adminredirect.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * @package    adminredirect
+ *
+ * @author     sharikov <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Database\DatabaseDriver;
+
+defined('_JEXEC') or die;
+
+/**
+ * Adminredirect plugin.
+ *
+ * @package  adminredirect
+ * @since    1.0
+ */
+class plgUserAdminredirect extends CMSPlugin
+{
+	/**
+	 * Application object
+	 *
+	 * @var    CMSApplication
+	 * @since  1.0
+	 */
+	protected $app;
+
+	/**
+	 * Database object
+	 *
+	 * @var    DatabaseDriver
+	 * @since  1.0
+	 */
+	protected $db;
+
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  1.0
+	 */
+	protected $autoloadLanguage = true;
+
+	public function onUserAfterLogin()
+    {
+        $url = $this->params->get('url', 'index.php?option=com_projects&view=todos');
+        if (!JFactory::getApplication()->isClient('site')) $this->app->redirect($url);
+    }
+
+	/**
+	 * onAfterInitialise.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterInitialise()
+	{
+		
+	}
+
+	/**
+	 * onAfterRoute.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRoute()
+	{
+	
+	}
+
+	/**
+	 * onAfterDispatch.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterDispatch()
+	{
+	
+	}
+
+	/**
+	 * onAfterRender.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRender()
+	{
+		// Access to plugin parameters
+		$sample = $this->params->get('sample', '42');
+	}
+
+	/**
+	 * onAfterCompileHead.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterCompileHead()
+	{
+	
+	}
+
+	/**
+	 * OnAfterCompress.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterCompress()
+	{
+	
+	}
+
+	/**
+	 * onAfterRespond.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRespond()
+	{
+	
+	}
+
+}
